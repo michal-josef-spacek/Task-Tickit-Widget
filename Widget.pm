00ee58d366290c8d6565f86379e8d2d5a03165a1
Michal Špaček <tupinek@gmail.com>
2015-02-12 19:29:21 +0100

    First version.

---
+++ Widget.pm
@@ -0,0 +1,109 @@
+package Task::Tickit::Widget;
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Version.
+our $VERSION = 0.01;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Task::Tickit::Widget - Install the Tickit::Widget modules.
+
+=head1 SYNOPSIS
+
+ cpanm Task::Tickit::Widget
+
+=head1 SEE ALSO
+
+L<Tickit-Widget>,
+L<Tickit-Widget-Border>,
+L<Tickit-Widget-Box>,
+L<Tickit-Widget-Breadcrumb>,
+L<Tickit-Widget-Button>,
+L<Tickit-Widget-Calendar-MonthView>,
+L<Tickit-Widget-CheckButton>,
+L<Tickit-Widget-Choice>,
+L<Tickit-Widget-Decoration>,
+L<Tickit-Widget-Entry>,
+L<Tickit-Widget-FileViewer>,
+L<Tickit-Widget-Fill>,
+L<Tickit-Widget-FloatBox>,
+L<Tickit-Widget-Frame>,
+L<Tickit-Widget-GridBox>,
+L<Tickit-Widget-HBox>,
+L<Tickit-Widget-HSplit>,
+L<Tickit-Widget-Layout-Desktop>,
+L<Tickit-Widget-Layout-Relative>,
+L<Tickit-Widget-LinearBox>,
+L<Tickit-Widget-LinearSplit>,
+L<Tickit-Widget-LogAny>,
+L<Tickit-Widget-Menu>,
+L<Tickit-Widget-MenuBar>,
+L<Tickit-Widget-Menu-Item>,
+L<Tickit-Widget-Placegrid>,
+L<Tickit-Widget-Progressbar>,
+L<Tickit-Widget-Progressbar-Horizontal>,
+L<Tickit-Widget-Progressbar-Vertical>,
+L<Tickit-Widget-RadioButton>,
+L<Tickit-Widget-ScrollBox>,
+L<Tickit-Widget-ScrollBox-Extent>,
+L<Tickit-Widget-Scroller>,
+L<Tickit-Widget-Scroller-Item>,
+L<Tickit-Widget-Scroller-Item-RichText>,
+L<Tickit-Widget-Scroller-Item-Text>,
+L<Tickit-Widget-SegmentDisplay>,
+L<Tickit-Widget-SparkLine>,
+L<Tickit-Widget-Spinner>,
+L<Tickit-Widget-Static>,
+L<Tickit-Widget-Statusbar>,
+L<Tickit-Widget-Statusbar-Clock>,
+L<Tickit-Widget-Statusbar-CPU>,
+L<Tickit-Widget-Statusbar-Memory>,
+L<Tickit-Widget-Statusbar-WidgetList>,
+L<Tickit-Widget-Tabbed>,
+L<Tickit-Widget-Tabbed-Ribbon>,
+L<Tickit-Widget-Table>,
+L<Tickit-Widget-Table-Cell>,
+L<Tickit-Widget-Table-Column>,
+L<Tickit-Widget-Table-HeaderCell>,
+L<Tickit-Widget-Table-HeaderRow>,
+L<Tickit-Widget-Table-Highlight>,
+L<Tickit-Widget-Table-Paged>,
+L<Tickit-Widget-Table-Row>,
+L<Tickit-Widget-Tree>,
+L<Tickit-Widget-VBox>,
+L<Tickit-Widget-VHBox>,
+L<Tickit-Widget-VSplit>,
+L<Tickit-Widgets>.
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Task-Tickit-Widget>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © 2015 Michal Špaček
+ Artistic License
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
